8d0de1bd007ccdc3ee58f6a33b6cafa3e4c29d89
Axzurey <66486198+Axzurey@users.noreply.github.com>
2023-01-10 20:25:21 -0500

    ehehe

--- lylac/services/SoundService.py
+++ lylac/services/SoundService.py
@@ -1,1 +1,18 @@
-#TODO: make sound service!
+import pygame
+
+
+class SoundService:
+    _sound_cache: dict[str, pygame.mixer.Sound] = {}
+
+    @staticmethod
+    def loadSound(alias: str, path: str):
+        SoundService._sound_cache[alias] = pygame.mixer.Sound(path);
+        return SoundService._sound_cache[alias];
+
+    @staticmethod
+    def playSound(alias: str, volume: float = 1):
+        if alias not in SoundService._sound_cache: raise Exception(f"{alias} is not a registered audio track");
+        
+        sound = SoundService._sound_cache[alias];
+        sound.set_volume(volume);
+        sound.play();
